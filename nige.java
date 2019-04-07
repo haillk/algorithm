66c53b99a96b606a2d685cfeecb28194e2039d75
haikuo <haillk@sina.cn>
2019-04-07 13:12:19 +0800

    Delete nige.java

--- nige.java
+++
@@ -1,9 +0,0 @@
-class TsetOurFor{
-	public static void main(String[] args)
-	{
-		int i = 47;
-		System.out.printf("%d\n",i);
-		System.out.printf("%x\n",i);
-		System.out.printf("%X\n",i);
-		}
-	}
